f33585dce6ff7689a82cf81ac62d97c2b77202ba
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-21 15:16:15 +0100

    update the lambda function to reflect task 3, completed in the AWS console

--- src/lambda_func.py
+++ src/lambda_func.py
@@ -1,16 +1,29 @@
 import random
 import logging
+import boto3
+import json
+from datetime import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client("s3")
+bucket = "dice-rolls-bucket-nc-2024"
 
 def lambda_handler(event, context):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
     dice_type = event.get("dice_type")
     dice_count = event.get("dice_count")
 
     num_sides = int(dice_type[1:])
     
     rolls = [random.randint(1, num_sides) for _ in range(dice_count)]
+    
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    key = f"results/{timestamp}.json"
+    s3.put_object(Bucket=bucket, Key=key, Body=json.dumps({
+        "roll(s)": rolls,
+        "timestamp": timestamp
+    }))
     
     logger.info(f"Generated roll(s): {rolls}")
     
