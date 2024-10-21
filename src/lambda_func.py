0e92ac232865131fdd5674e4c12bd312212d9610
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-21 10:48:38 +0100

    verify passing test for task 1

---
+++ src/lambda_func.py
@@ -0,0 +1,19 @@
+import random
+import logging
+
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    dice_type = event.get("dice_type")
+    dice_count = event.get("dice_count")
+
+    num_sides = int(dice_type[1:])
+    
+    rolls = [random.randint(1, num_sides) for _ in range(dice_count)]
+    
+    logger.info(f"Generated random integer(s): {rolls}")
+    
+    return {
+        "dice_count": rolls
+    }
